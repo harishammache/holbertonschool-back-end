5cc6826472bf3647893136cbb03ddd79ef768faf
harishammache <hammache_haris@hotmail.fr>
2024-04-15 11:28:14 +0000

    pycodestyle

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
+from sys import argv
+import requests
 """
     using the REST API for a given employee ID
     returns informations about his TODO list progress
 """
 
 
-from sys import argv
-import requests
-
-
 def employee_id(employe_id):
     """returns information about his/her TODO list progress."""
-
     url = f"https://jsonplaceholder.typicode.com/todos?userId={employe_id}"
     url_name = f"https://jsonplaceholder.typicode.com/users/{employe_id}"
     response_name = requests.get(url_name)

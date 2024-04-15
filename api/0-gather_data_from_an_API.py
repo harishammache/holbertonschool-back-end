fab1b31d326e6b1ac55df16b6532100ef4ef3d6a
harishammache <hammache_haris@hotmail.fr>
2024-04-15 11:48:18 +0000

    change name of variable

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -16,25 +16,23 @@ def employee_id(employe_id):
     url_name = f"https://jsonplaceholder.typicode.com/users/{employe_id}"
     response_name = requests.get(url_name)
     employee_data = response_name.json()
-    employee_name = employee_data['name']
+    EMPLOYEE_NAME = employee_data['name']
 
     response_todo = requests.get(url)
     todos = response_todo.json()
 
-    num_completed_tasks = 0
-    total_tasks = 0
+    NUMBER_OF_DONE_TASKS = 0
     for completed in todos:
         if completed['completed']:
-            num_completed_tasks += 1
-    total_tasks = len(todos)
+            NUMBER_OF_DONE_TASKS += 1
+    TOTAL_NUMBER_OF_TASKS = len(todos)
 
-    print(f"Employee {employee_name} is done with tasks\
-({num_completed_tasks}/{total_tasks}):")
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks\
+({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
     for completed in todos:
         if completed['completed']:
             print(f"\t {completed['title']}")
 
 
 if __name__ == "__main__":
-    employe = (int(argv[1]))
-    employee_id(employe)
+    employee_id(int(argv[1]))
